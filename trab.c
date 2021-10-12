ec29b4ca2a7ccd4411844af772671728b0bf6ea5
GGM <ra116843@uem.br>
2021-10-11 23:31:10 -0300

    att

--- trab.c
+++ trab.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string.h>
+#include <stdlib.h>
+#define COMP_REG 64
 
 int input(char str[], int size) {
     int i = 0;
@@ -16,67 +19,64 @@ int input(char str[], int size) {
     return i;
 }
 
-void importação(FILE * dados){
+short leia_reg(char buffer[], int tam, FILE *entrada) {
+
+    short comp_reg;
     
+    if (fread(&comp_reg, sizeof(short), 1, entrada) == 0) {
+        return 0;
+    }
+
+    if (comp_reg < tam) {
+        fread(buffer, sizeof(char), comp_reg, entrada);
+        buffer[comp_reg] = '\0';
+        return comp_reg;
+    } else {
+        return 0;
+    }
 }
+
+struct{
+    int cont_reg;
+}cab; // cabeçalho do arquivo ( 4 bytes)
+
+
+//void importação(FILE * dados){
+    
+
 
 int main(int argc, char *argv[]) {
 
     FILE * arq;
-    FILE * data;
+    //FILE * dados;
     char nome_arq[20];
+    char buffer[COMP_REG + 1];
+    int comp_reg, i, j, byte_offset;
+    char *campo;
+    char nome_arq[20];
+    char campo[20];
+    short comp_reg;
+    char *nome_campos[] = {"Nome", "Endereco", "Cidade", "Estado", "CEP"};
 
     
     if (argc == 3 && strcmp(argv[1], "-i") == 0) {
             printf("Informe o nome do arquivo de registros: ");
             input(nome_arq, 20);
 
-        if ((arq = fopen(nome_arq, "rb")) == NULL){
-            fprintf(stderr, "Erro, nao foi possivel abrir o arquivo '%s'", nome_arq);
-            exit(EXIT_FAILURE);
-        }   
+       if ((arq = fopen(nome_arq, "r+b")) == NULL) {
+        arq = fopen(nome_arq, "w+b");
+        cab.cont_reg = 0;
+        fwrite(&cab, sizeof(cab), 1, arq);
+    } else {
+        fread(&cab, sizeof(cab), 1, arq);
+    }
+   
+
+                                          
     
-        printf("Modo de importacao ativado ... nome do arquivo = %s\n", argv[2]);
-
-        if ((data = fopen("dados.dat", "w+b"))==NULL)
-            perror("O arquivo não pode ser aberto");
-
-        
-        // importacao(argv[2]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    } else if (argc == 3 && strcmp(argv[1], "-e") == 0) {
+    
+    
+ } else if (argc == 3 && strcmp(argv[1], "-e") == 0) {
 
         printf("Modo de execucao de operacoes ativado ... nome do arquivo = %s\n", argv[2]);
         // executa_operacoes(argv[2]);
